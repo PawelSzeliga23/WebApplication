e0051805b79081903fe0fc780645bfb5db7a2c6a
s27030 <s27030@pjwstk.edu.pl>
2024-04-08 22:15:31 +0200

    Renamed project and added DataBase.cs and Animal.cs

---
+++ WebApiProject/DataBase/DataBase.cs
@@ -0,0 +1,15 @@
+﻿using WebApiProject.Models;
+
+namespace WebApiProject.DataBase;
+
+public class DataBase
+{
+    public static List<Animal> Animlas = new()
+    {
+        new Animal{Id = 1,Name = "Reksio",Type = "Dog",Mass = 4.5,FurColor = "Black"},
+        new Animal{Id = 2, Name = "Whiskers", Type = "Cat", Mass = 3.2, FurColor = "White"},
+        new Animal{Id = 3, Name = "Buddy", Type = "Dog", Mass = 7.8, FurColor = "Brown"},
+        new Animal{Id = 4, Name = "Mittens", Type = "Cat", Mass = 2.7, FurColor = "Gray"},
+        new Animal{Id = 5, Name = "Daisy", Type = "Dog", Mass = 6.1, FurColor = "Golden"},
+    };
+}
---
+++ WebApiProject/Models/Animal.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApiProject.Models;
+
+public class Animal
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public double Mass { get; set; }
+    public string FurColor { get; set; }
+}
